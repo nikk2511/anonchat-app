9275f901d9acd1370fe02d902b24affc19c86eb3
nikk2511 <nikhil95696@gmail.com>
2025-07-28 16:44:08 +0530

    \Final fixes for import syntax"

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Navbar } from '@/components/Navbar';
+import Navbar from '@/components/Navbar';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
